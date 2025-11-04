6fe16214636c58f5c146b8b3abfde4a684a13840
“RT527” <rafi.talukder7@gmail.com>
2025-11-04 07:21:12 -0500

    Update sql

--- load_pets.py
+++ load_pets.py
@@ -1,5 +1,6 @@
 # Rafi Talukder Assignment_9
 import sqlite3
+"""---------------------------------------------------------------------------------"""
 
 if __name__ == "__main__":
     print("Running load_pets.py")
